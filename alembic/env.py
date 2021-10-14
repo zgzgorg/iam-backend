236a1657eacc09f29c15b509c3d5093e89dc3679
William Chen <kis87988@gmail.com>
2021-10-13 22:13:20 -0700

    fix: the model user rename to account (#45)
    - rename all models
    - regenerate the init schema part
    - fix the hook for Black in alembic
    - fix the test as well

--- alembic/env.py
+++ alembic/env.py
@@ -19,7 +19,7 @@ fileConfig(config.config_file_name)
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = zgiam.models.BASE.metadata
+target_metadata = zgiam.models.base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
